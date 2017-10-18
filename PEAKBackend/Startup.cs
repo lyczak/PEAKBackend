8763395402a71ac3df19eea441d442d440f0f509
Gabe Lyczak <glyczak@gmail.com>
2017-10-18 07:43:16 -0400

    Add project files.

---
+++ PEAKBackend/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PEAKBackend.Startup))]
+namespace PEAKBackend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
